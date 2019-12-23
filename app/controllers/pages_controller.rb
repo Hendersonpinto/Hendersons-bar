d215c62a0982d1454ec5ef59ea24e90b162540ab
Henderson Pinto <hendersoncode@gmail.com>
2019-12-24 00:54:47 +0100

    Added video, golden button and responsiveness

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: :home
   def home
     # render :layout => 'home'
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.new(strong_params)
+    @email.save
+  end
+
+  private
+
+  def strong_params
+    params.require(:email).permit(:address, :name)
   end
 end
