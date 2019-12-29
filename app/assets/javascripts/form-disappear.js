b7b01450756985e471849e011d97d57ddd95a7a0
Henderson Pinto <hendersoncode@gmail.com>
2019-12-29 14:23:04 +0100

    Fixed the javascript callback on home and added search bar in nav for long screens

--- app/assets/javascripts/form-disappear.js
+++ app/assets/javascripts/form-disappear.js
@@ -1,15 +1,15 @@
 console.log("Hello from form-disappear.js!");
-const element = document.querySelector("#disappearing-form");
-const button = document.getElementById("formbutton");
+var element = document.querySelector("#disappearing-form");
+var button = document.getElementById("formbutton");
 
-
+if (button){
 button.addEventListener("click", (event) =>{
   console.log("You clicked on the button");
   console.log(event);
   console.log(event.currentTarget);
   // element.classList.add("hidden");
   element.innerHTML = "<p>YOU HAVE BEEN SUCCESSFULLY SIGNED UP </p>";
-})
+})}
 
 
 // // setTimeout (()=> {
